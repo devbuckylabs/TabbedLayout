76cd2ecf857fb029af663e6b45c7b39e217faa31
devbuckylabs <dev.buckylabs@gmail.com>
2019-03-24 11:48:12 +0530

    Removed Dark mode

--- app/src/main/java/com/buckylabs/tabbedlayoutexp/MainActivity.java
+++ app/src/main/java/com/buckylabs/tabbedlayoutexp/MainActivity.java
@@ -42,29 +42,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
-            setTheme(R.style.DarkTheme);
-        } else {
-            setTheme(R.style.AppTheme);
-
-        }
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         preferences= PreferenceManager.getDefaultSharedPreferences(this);
-        boolean isDark = preferences.getBoolean("example_switch", false);
 
-if(isDark){
-
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-
-}else{
-
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-
-
-}
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
